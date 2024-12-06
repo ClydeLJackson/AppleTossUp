a6a4af7a37b8c5b3111a979d69fa6d088e4569a9
Clyde Jackson <jacksonclyde818@gmail.com>
2024-12-06 10:00:30 -0500

    Initial Commit

---
+++ AppleTossUp/AppleTossUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTossUpApp.swift
+//  AppleTossUp
+//
+//  Created by Clyde Jackson on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AppleTossUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleTossUpTests/AppleTossUpTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppleTossUpTests.swift
+//  AppleTossUpTests
+//
+//  Created by Clyde Jackson on 12/6/24.
+//
+
+import Testing
+@testable import AppleTossUp
+
+struct AppleTossUpTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
